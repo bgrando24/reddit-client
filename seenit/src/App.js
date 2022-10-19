3609ba6cf784e509a76f26dce32615c653877e34
Brandon <bgrando24@gmail.com>
2022-10-19 19:34:14 +1100

    Updating

---
+++ seenit/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Navbar } from './components/navbar/Navbar';
+
+function App() {
+  return (
+
+// forcing update
+
+    <div>
+      <Navbar />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ seenit/src/components/navbar/Navbar.js
@@ -0,0 +1,8 @@
+
+export function Navbar() {
+    return (
+        <div className="bg-gray-500">
+            <p>Login</p>
+        </div>
+    )
+}

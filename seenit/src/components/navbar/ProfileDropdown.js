0e157ae4a5e0ebdc9b65395895f539e9da2a1cd7
Brandon <bgrando24@gmail.com>
2022-10-20 20:17:59 +1100

    Added items to profile drop-down menu

--- seenit/src/components/navbar/ProfileDropdown.js
+++ seenit/src/components/navbar/ProfileDropdown.js
@@ -1,11 +1,36 @@
-// 252px wide 600px high
+
+import {MdOutlineAccountCircle} from 'react-icons/md';
+import {IoSettingsOutline, IoLogOutOutline} from 'react-icons/io5';
+import {AiOutlineInfoCircle} from 'react-icons/ai';
+
+
+// Array to hold the options available in the profile menu
+const profileMenuItems = [
+    {name: "Profile",           icon: <MdOutlineAccountCircle />,   path: "/profile"},
+    {name: "Settings",          icon: <IoSettingsOutline />,        path: "/settings"},
+    {name: "About this app",    icon: <AiOutlineInfoCircle />,      path: "/about"},
+    {name: "Logout",            icon: <IoLogOutOutline />,          path: "/logout"},
+];
+
+
 
 export function ProfileDropdown({ menuOpen }) {
 
     if (menuOpen) {
         return (
             <div className="fixed right-5 top-10 w-[252px] h-[600px] bg-white border border-gray-400 rounded-md z-10 duration-150">
-                Menu
+                <div className='flex flex-col justify-around h-full'>
+                    {
+                        profileMenuItems.map(item => {
+                            return (
+                                <div key={item.name} className='flex items-center bg-gray-100 h-[13%] mx-1 rounded-lg cursor-pointer hover:scale-95 hover:bg-gray-300 duration-150'>
+                                    <span className='ml-4'>{item.icon}</span>
+                                    <span className='ml-2'>{item.name}</span>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
         )
 
